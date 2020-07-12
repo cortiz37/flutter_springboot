bbf1630a09baec4dbb47d0d4825a24d0c7b07e0a
carlos pupo <carlos.pupo@jobsity.io>
2020-07-12 15:59:26 -0500

    tests for app & controller.

---
+++ rest_api/src/test/java/com/sample/server/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.sample.server;
+
+import com.sample.server.controller.ElementController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Autowired
+    private ElementController elementController;
+
+    @Test
+    public void contextIsPresent() throws Exception {
+        assertNotNull(elementController);
+    }
+}
