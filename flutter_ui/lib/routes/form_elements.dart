e0fb448fbf2c074da9b6f72abcc8f0eb1c8fc057
carlos pupo <carlos.pupo@jobsity.io>
2020-07-20 16:03:27 -0500

    subviews per route.

---
+++ flutter_ui/lib/routes/form_elements.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FormElements extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: SizedBox(
+              width: 400,
+              child: Card(
+                child: Text('HELLO!'),
+              ),
+            ),
+          ),
+        ));
+  }
+}
