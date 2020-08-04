2bfb432b2e69ccf6eb8d53ec50690ecf9fefbdbb
cortiz37 <cortiz37@gmail.com>
2020-08-03 19:10:25 -0500

    form element init.

--- flutter_ui/lib/routes/form_elements.dart
+++ flutter_ui/lib/routes/form_elements.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
 
-class FormElements extends StatelessWidget {
+class FormElements extends StatefulWidget {
   final AppBar appBar;
 
   FormElements({this.appBar});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: this.appBar,
-        backgroundColor: Colors.grey[200],
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: SizedBox(
-              width: 400,
-              child: Card(
-                child: Text('Form'),
+  _FormElements createState() => _FormElements();
+}
+
+class _FormElements extends State<FormElements> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildForm(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text('SAMPLE')],
               ),
             ),
           ),
-        ));
+          Container(
+            height: 40,
+            width: double.infinity,
+            child: FlatButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              onPressed: () => {},
+              child: Text('Accept'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: widget.appBar,
+        backgroundColor: Colors.grey[200],
+        body: this._buildForm(context));
   }
 }
