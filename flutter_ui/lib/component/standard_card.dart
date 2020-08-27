98aaad2e9cec7f9443f013799f9d267e138dfbd2
cortiz37 <cortiz37@gmail.com>
2020-08-27 08:50:56 -0500

    standard component for messages.

---
+++ flutter_ui/lib/component/standard_card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class StandardCard extends StatelessWidget {
+  final String text;
+
+  StandardCard({
+    @required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(Icons.error_outline, size: 16, color: Colors.red),
+            SizedBox(height: 30),
+            Text(this.text, style: Theme.of(context).textTheme.subtitle1),
+          ],
+        ),
+      ),
+    );
+  }
+}
